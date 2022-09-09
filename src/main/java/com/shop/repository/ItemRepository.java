c1e02a511edfc470540be68ccbd0c88924a83d28
Tublanx <rlgus03453@naver.com>
2022-09-09 22:48:48 +0900

    Project Setting

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,26 @@
+package com.shop.repository;
+
+import com.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemName(String itemName);
+
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(nativeQuery = true, value = "select * from Item i where i.item_detail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+}
---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,110 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+        item.setItemName("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNameTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemName("테스트 상품1");
+        for (var item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 or 상품 상세설명 테스트")
+    public void findByItemNameOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNameOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (var item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (var item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanOrderByPriceTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (var item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명1");
+        for (var item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("navtiveQuery 속성을 이용한 상품 조회 테스트")
+    public void findByItemDetailByNativeTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명1");
+        for (var item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+}
