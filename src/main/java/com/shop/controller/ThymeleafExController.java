994de0ef4b85d9f2f61ab30f7ab13c648f4eb565
Tublanx <rlgus03453@naver.com>
2022-09-11 14:30:15 +0900

    th:if, th:unless example code

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -49,4 +49,22 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx03";
     }
 
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1; i<=10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemName("테스트 상품" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+
 }
