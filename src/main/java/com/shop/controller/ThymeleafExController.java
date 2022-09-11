2c19bf100098e6a6e24d0e5cdb5a7b780c649aef
Tublanx <rlgus03453@naver.com>
2022-09-11 14:40:45 +0900

    add th:href example code (relay parameters)

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -67,4 +67,16 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx04";
     }
 
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
+
+    @GetMapping(value = "/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
+
 }
