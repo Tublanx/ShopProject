ec36a2edf7e2814682a0da2d5273f338a9bf7cd4
Tublanx <rlgus03453@naver.com>
2022-09-17 18:20:10 +0900

    add validation

--- src/main/java/com/shop/dto/MemberFormDto.java
+++ src/main/java/com/shop/dto/MemberFormDto.java
@@ -15,7 +15,7 @@ public class MemberFormDto {
     @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
-    @NotEmpty(message = "이메일을 필수 입력 값입니다.")
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
