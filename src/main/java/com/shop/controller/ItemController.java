e0e50871644fa99477689d4c21f1877b01a1b7f7
Tublanx <rlgus03453@naver.com>
2022-09-30 18:16:58 +0900

    add itemForm route controller

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,2 +1,14 @@
-package com.shop.controller;public class ItemController {
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+
+    @GetMapping(value = "/admin/item/new")
+    public String itemForm() {
+        return "/item/itemForm";
+    }
+
 }
