057eb063ad31eaa099e9dee0bf9f8911d9e51e99
Tublanx <rlgus03453@naver.com>
2022-10-02 14:23:20 +0900

    test cart entity

--- src/test/java/com/shop/entity/CartTest.java
+++ src/test/java/com/shop/entity/CartTest.java
@@ -1,4 +1,64 @@
+package com.shop.entity;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.repository.CartRepository;
+import com.shop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
 class CartTest {
-  
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Member createMember() {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("rlgus03453@naver.com");
+        memberFormDto.setName("이기현");
+        memberFormDto.setAddress("서구 완정로 92번길 41");
+        memberFormDto.setPassword("1234");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("장바구니 회원 엔티티 매핑 조회 테스트")
+    public void findCartAndMemberTest() {
+        Member member = createMember();
+        memberRepository.save(member);
+
+        Cart cart = new Cart();
+        cart.setMember(member);
+        cartRepository.save(cart);
+
+        entityManager.flush(); // 영속성 컨텍스트에 데이터 저장 후 트랜잭션이 끝날 때 flush를 호출해 DB에 반영
+        entityManager.clear(); // ""                                                            조회
+
+        Cart savedCart = cartRepository.findById(cart.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        assertEquals(savedCart.getMember().getId(), member.getId());
+    }
+
 }
