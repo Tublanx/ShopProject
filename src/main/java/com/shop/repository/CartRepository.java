05627b4898695747a75588898ee8c47d383f1c69
Tublanx <rlgus03453@naver.com>
2022-10-02 14:23:13 +0900

    create cart repository

--- src/main/java/com/shop/repository/CartRepository.java
+++ src/main/java/com/shop/repository/CartRepository.java
@@ -3,5 +3,5 @@ package com.shop.repository;
 import com.shop.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Cart, Long> { 
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
