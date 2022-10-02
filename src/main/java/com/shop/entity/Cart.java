077fdaba5c0e3475ce52b19ff7a74accbd9d9ecb
Tublanx <rlgus03453@naver.com>
2022-10-02 14:15:12 +0900

    create cart entity

---
+++ src/main/java/com/shop/entity/Cart.java
@@ -0,0 +1,25 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_id")
+    private Long id;
+
+    @OneToOne // 회원 엔티티와 일대일 매핑
+    @JoinColumn(name = "member_id") // 매핑할 외래키 지정
+    private Member member;
+
+}
