dd3f8954c4fb97a05ab38f221ba624efad724210
Tublanx <rlgus03453@naver.com>
2022-10-10 16:03:28 +0900

    create order item entity

---
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,41 @@
+package com.shop.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "order_item_id")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "item_id")
+	private Item item;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	private int orderPrice; // 주문가격
+	
+	private int count; // 수량
+	
+	private LocalDateTime regTime;
+	
+	private LocalDateTime updateTime;
+	
+}
