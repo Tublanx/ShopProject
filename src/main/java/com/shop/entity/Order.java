6cc6bbfcd00f2cd66e765c19af01dff048c6c278
Tublanx <rlgus03453@naver.com>
2022-10-20 23:08:14 +0900

    set orphanRemoval true

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -26,26 +26,26 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Order {
-		
+
 	@Id
 	@GeneratedValue
 	@Column(name = "order_id")
 	private Long id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "member_id")
 	private Member member;
-	
+
 	private LocalDateTime orderDate; // 주문일
-	
+
 	@Enumerated(EnumType.STRING)
 	private OrderStatus orderStatus; // 주문상태
-	
-	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<OrderItem> orderItems = new ArrayList<>();
-	
+
 	private LocalDateTime regTime;
-	
+
 	private LocalDateTime updateTime;
-	
+
 }
